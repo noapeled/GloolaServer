7c8a22c2bf0748b3cb0ae8384fbb27dc5c4fffc0
noapeled@gmail.com <noapeled@gmail.com>
2017-04-15 12:48:05 +0300

    Change DATABASES settings to mongo DB

--- helloapp/helloapp/settings.py
+++ helloapp/helloapp/settings.py
@@ -74,11 +74,18 @@ WSGI_APPLICATION = 'helloapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'my_database'
+   }
 }
 
 
