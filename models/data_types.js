290004f5474cadb7207fe436af84b36d83fe7cb2
noapeled@gmail.com <noapeled@gmail.com>
2017-04-15 20:18:34 +0300

    Medicine model

---
+++ models/data_types.js
@@ -0,0 +1,9 @@
+/**
+ * Created by noa on 15/04/17.
+ */
+
+exports.LimitedLengthString = {
+    type: String,
+    min: 0,
+    max: 2048
+};
---
+++ models/medicine.js
@@ -0,0 +1,21 @@
+/**
+ * Created by noa on 15/04/17.
+ */
+
+var LimitedLengthString = require('./data_types').LimitedLengthString
+var mongoose = require('mongoose');
+var _ = require('lodash');
+
+var MedicineSchema = mongoose.Schema({
+    medicine_id: LimitedLengthString,
+    medicine_names: [LimitedLengthString],
+    images: [],
+    route_of_administration: _.merge(LimitedLengthString, {
+        enum: ['oral', 'intravenous']
+    }),
+    dosage_form: LimitedLengthString,
+    manufacturer: LimitedLengthString,
+    mg_per_dosage: { type: Number, min: [0.001, 'Too low mg per dosage'] }
+});
+
+var Medicine = mongoose.model('Medicine', MedicineSchema);
