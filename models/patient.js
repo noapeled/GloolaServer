420f5e44e8f45429ddafe0aeb16919df27e3d474
noapeled@gmail.com <noapeled@gmail.com>
2017-04-16 13:57:43 +0300

    Patient Model

---
+++ models/patient.js
@@ -0,0 +1,42 @@
+/**
+ * Created by noa on 16/04/17.
+ */
+
+var data_types = require('./data_types');
+var mongoose = require('mongoose');
+require('mongoose-type-email');
+
+function regexpForCronStyleValue(numbersString) {
+    return new RegExp('^([*])|((' + numbersString + ',)*' + numbersString + ')$');
+}
+
+var PatientSchema = data_types.createSchema({
+    patient_id: { type: String, required: true },
+    name: {
+        type: [String],
+        validate: { validator: function(v) { return v.length >= 2 }, message: 'name must consist of at least forname and surname' }
+    },
+    birthdate: Date,
+    hmo: { type: String, enum: ['clalit', 'maccabi', 'meuhedet', 'leumit', null], default: null },
+    email: mongoose.SchemaTypes.Email,
+    medication: [{
+        medicine_id: { type: String, required: true },
+        dosage_size: {
+            type: Number,
+            required: true,
+            validate: { validator: function(v) { return v > 0 }, message: 'dosage_size must be positive' }
+        },
+        frequency: [{
+            day_of_week: { type: String, match: regexpForCronStyleValue('[1-7]') },
+            month_of_year: { type: String, match: regexpForCronStyleValue('[1-12]') },
+            day_of_month: { type: String, match: regexpForCronStyleValue('[1-31]') },
+            hour: { type: String, match: regexpForCronStyleValue('[0-59]') },
+            minute: { type: String, match: regexpForCronStyleValue(('[0-23]')) }
+        }]
+    }]
+});
+
+exports.PatientSchema = PatientSchema;
+
+var Patient = mongoose.model('Patient', PatientSchema);
+exports.Patient = Patient;
