fd032a0977552bc42d37ce1fc0071af07dee8f4c
noapeled@gmail.com <noapeled@gmail.com>
2017-04-18 20:23:32 +0300

    Fix tests without mandatory email

--- models/user-spec.js
+++ models/user-spec.js
@@ -14,18 +14,18 @@ describe('User', function() {
     it('should fail on creation of user without required fields', function() {
         var User = require('./user.js');
         var error = new User().validateSync();
-        expect(_.isEqual(_.keys(error.errors).sort(), ['username', 'password'].sort())).to.be.true;
+        expect(_.isEqual(_.keys(error.errors).sort(), ['username', 'password', 'email'].sort())).to.be.true;
     });
 
     it('should succeed on creation of user with only required fields', function() {
         var User = require('./user.js');
-        var user = new User({ username: 'foofoouser', password: 'barbarpassword' });
+        var user = new User({ username: 'foofoouser', password: 'barbarpassword', email: 't@t.com' });
         expect(user.validateSync()).to.be.undefined;
     });
 
     it('should have default basic properties for user created with only required fields', function() {
         var User = require('./user.js');
-        var user = new User({ username: 'foofoouser', password: 'barbarpassword' });
+        var user = new User({ username: 'foofoouser', password: 'barbarpassword', email: 't@t.com' });
         expect(user.hidden).to.be.false;
         expect(user.creation_date).to.exist;
     });
@@ -35,6 +35,7 @@ describe('User', function() {
         var user = new User({
             username: 'foofoouser',
             password: 'barbarpassword',
+            email: 't@t.com',
             patients: ['1234', '6677']
         });
         expect(user.validateSync()).to.be.undefined;
