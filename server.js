ea4f1373d66b6307acee2e4bceb8fec4f89f46c1
Noa Peled <noapeled@gmail.com>
2017-05-18 15:43:55 +0200

    Bring back code changes from start of May

--- server.js
+++ server.js
@@ -15,73 +15,22 @@ var bodyParser  =   require("body-parser");
 var router      =   express.Router();
 var mongoose    = require('mongoose');
 
-var ImageModel = require('./models/image');
-var PatientModel = require('./models/patient');
-var MedicineModel = require('./models/medicine');
-var UserModel = require('./models/user');
-
 var config = {
     port: 3000,
     auth: {
+        tokenFeatureFlag: false,
         adminPassword: 'gloola123!',
         serverSecret: 'This is a secret string for signing tokens',
         tokenValidity: "1day"
     }
 };
 
-function getByEntityId(req, res) {
-    var modelNameToIdentifier = {
-        User: 'username',
-        Medicine: 'medicine_id',
-        Patient: 'patient_id',
-        Image: 'image_id'
-    };
-    var model = _.startCase(req.params.collection);
-    var identifier = modelNameToIdentifier[model];
-    var query = _.fromPairs([[identifier, req.params.entity_id]]);
-    mongoose.models[model].find(query, function(err, data) {
-        if (data && data.length <= 0) {
-            res.status(400).json({ error: true, message: "No " + req.params.collection + " with " + identifier + " " + req.params.entity_id})
-        } else {
-            res.json({
-                error: err ? err : false,
-                "message": err ? "Error fetching data" : data[0]
-            });
-        }
-    });
-}
-
-function updateExistingImage(req, res) {
-    mongoose.models.Image.find({ image_id: req.body.image_id }, function(err, data) {
-        var existingImage = data[0];
-        _.forOwn(_.omit(req.body, 'image_id'), function (value, key) {
-            existingImage[key] = value;
-        });
-        existingImage.save(function (err) {
-            res.status(statusCode(err)).json({
-                "error": err ? err : false,
-                "message": (err ? "Error updating image " : "Updated image ") + req.body.image_id
-            });
-        });
-    })
-}
-
-function createNewImage(req, res) {
-    mongoose.models.Image.find({ image_id: req.body.image_id }, function(err, data) {
-        var newImage = new ImageModel(req.body);
-        newImage.save(function(err) {
-            res.status(statusCode(err)).json({
-                "error" : err ? err : false,
-                "message" : (err ? "Error creating image " : "Created image ") + req.body.image_id
-            });
-        });
-    });
-}
-
-function statusCode(err) {
-    var UNIQUE_KEY_ERROR_CODE = 11000;
-    return _.get(err, 'name') === 'ValidationError' || _.get(err, 'code') === UNIQUE_KEY_ERROR_CODE ? 400 : 500;
-}
+var modelNameToIdentifier = {
+    User: 'username',
+    Medicine: 'medicine_id',
+    Patient: 'patient_id',
+    Image: 'image_id'
+};
 
 function getAllEntitiesInCollection(req, res) {
     if (req.params.collection === 'image') {
@@ -209,81 +158,80 @@ function verifyToken(req, res, next) {
     }
 }
 
-function getAllUsers(req, res) {
-    mongoose.models.User.find({ }, function(err, data) {
-        res.json({
-            "message" : err ? "Error fetching data" : data,
-            error: err ? err : false
+function authorizeAccessToEntireCollection(req, res, next) {
+    return req.decodedToken.username === 'admin' || _.includes(['medicine', 'image'], req.params.collection) ?
+        next() :
+        res.status(403).json({
+            err: true,
+            message: 'User ' + req.decodedToken.username + ' is not authorized to access all ' + req.params.collection
         });
-    });
 }
 
-function authenticateAdmin(req, res) {
-    if (req.body.password !== config.auth.adminPassword) {
-        res.status(400).json({ error: true, message: 'Wrong password for admin' });
+function authorizeAccessToUserEntity(req, res, next) {
+    return _.includes(['admin', req.params.userId], req.decodedToken.username) ?
+        next() :
+        res.status(403).json({
+            error: true,
+            message: 'User ' + req.decodedToken.username + ' is not authorized to access any other user' });
+}
+
+function authorizeAccessToPatientEntity(req, res, next) {
+    var username = req.decodedToken.username;
+    if (username === 'admin') {
+        return next();
     } else {
-        res.json({
-            error: false,
-            message: 'Successfully authenticated.',
-            token: jwt.sign(
-                { username: "admin" }, config.auth.serverSecret, { expiresIn: config.auth.tokenValidity })
+        mongoose.models.User.findOne({ username: username }, function(err, user) {
+            if (err) {
+                res.status(500).json({ error: err, mesage: "Error fetching user " + username});
+            } else if (!user) {
+                res.status(400).json({ error: true, message: 'User ' + username + ' not found.' });
+            } else if (!_.includes(user.patients, req.params.patientId)) {
+                res.status(403).json({
+                    error: true,
+                    message: 'User ' + username + ' is not authorized to access patient ' + req.params.patientId });
+            } else {
+                next();
+            }
         });
     }
 }
 
-function authenticateUserNotAdmin(req, res) {
-    UserModel.findOne({ username: req.body.username }, function(err, user) {
-        if (err) {
-            res.status(500).json({ error: err, mesage: "Error fetching user " + req.body.username});
-        } else if (!user) {
-            res.status(400).json(
-                { error: true, message: 'User ' + req.body.username + ' not found.' });
-        } else if (user.password !== req.body.password) {
-            res.status(400).json(
-                { error: true, message: 'Wrong password for user ' + req.body.username });
-        } else {
-            res.json({
-                error: false,
-                message: 'Successfully authenticated.',
-                token: jwt.sign(
-                    { username: user.username }, config.auth.serverSecret, { expiresIn: config.auth.tokenValidity })
-            });
-        }
-    });
+function authorizeCreationOfEntity(req, res, next) {
+    return req.decodedToken.username === 'admin' ?
+        next() :
+        res.status(403).json({
+            error: true,
+            message: 'User ' + req.decodedToken.username + ' is not authorized to create new entities' });
 }
 
-function authenticate(req, res) {
-    return req.body.username === 'admin' ? authenticateAdmin(req, res) : authenticateUserNotAdmin(req, res);
+function serverMain() {
+    if (config.auth.tokenFeatureFlag) {
+        router.route("/authenticate")
+            .post(authenticate);
+        router.use(verifyToken);
+        router.route("/:collection")
+            .put(authorizeCreationOfEntity)
+            .all(authorizeAccessToEntireCollection);
+        router.route("/user/:userId")
+            .all(authorizeAccessToUserEntity);
+        router.route("/patient/:patientId")
+            .all(authorizeAccessToPatientEntity);
+    }
+
+    router.route('/:collection')
+        .get(getAllEntitiesInCollection)
+        .post(updateExistingEntity)
+        .put(createNewEntity);
+
+    router.route('/:collection/:entityId')
+        .get(getByEntityId);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({"extended": false}));
+    app.use('/', router);
+
+    app.listen(config.port);
+    console.log("Listening to PORT " + config.port);
 }
 
-router.route("/authenticate")
-    .post(authenticate);
-
-router.route("/image")
-    .post(updateExistingImage)
-    .put(createNewImage);
-
-router.route("/user")
-    .get(getAllUsers)
-    .post(updateExistingUser)
-    .put(createNewUser);
-
-router.route("/medicine")
-    .get(getAllMedicine)
-    .post(updateExistingMedicine)
-    .put(createNewMedicine);
-
-router.route("/patient")
-    .get(getAllPatients)
-    .post(updateExistingPatient)
-    .put(createNewPatient);
-
-router.route('/:collection/:entity_id')
-    .get(getByEntityId);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended" : false}));
-app.use('/', router);
-
-app.listen(config.port);
-console.log("Listening to PORT " + config.port);
+serverMain();
