0db00e5d979eed6797dd2c9afe1923724f6ebed7
Noa Peled <noapeled@gmail.com>
2017-05-20 19:50:15 +0200

    More wonderful stuff

--- test_server.js
+++ test_server.js
@@ -2,23 +2,25 @@
  * Created by inon on 5/20/17.
  */
 
-'use strict';
-
 var testDbName = 'temporaryTestDb';
 
 require('./server').serverMain(testDbName);
-// TODO: eventually delete test database through mongo.
 
 var http = require('http');
 var expect = require('chai').expect;
 
-function postToServer(path, postBody, callbackOnResponseData) {
+adminToken = '';
+
+function putOrPostToServer(jwtToken, method, path, postBody, callbackOnResponseData) {
     var postOptions = {
         path: path,
         host: 'localhost',
         port: '3000',
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-ACCESS-TOKEN': jwtToken
+        }
     };
 
     var postReq = http.request(postOptions, function(res) {
@@ -31,18 +33,43 @@ function postToServer(path, postBody, callbackOnResponseData) {
     postReq.end();
 }
 
-function createNewUser(name, email, password) {
-
+function createNewUserAsAdmin(name, email, password) {
+    // console.log('createNewUserAsAdmin started with admin token ' + adminToken);
+    putOrPostToServer(
+        adminToken,
+        'PUT',
+        '/user',
+        { name: name, email: email, password: password },
+        function (chunk) {
+            var jsonBody = JSON.parse(chunk);
+            console.log(jsonBody);
+            expect(jsonBody.error).to.be.false;
+        }
+    )
 }
 
-postToServer(
-    '/authenticate',
-    { userid: 'admin', password: 'gloola123!'},
-    function (chunk) {
-        var jsonBody = JSON.parse(chunk);
-        console.log(jsonBody);
-        expect(jsonBody.error).to.be.false;
-        expect(jsonBody.message).to.be.defined;
-        expect(jsonBody.token).to.be.defined;
-    }
-);
+
+function getAdminToken() {
+    putOrPostToServer(
+        '',
+        'POST',
+        '/authenticate',
+        {userid: 'admin', password: 'gloola123!'},
+        function (chunk) {
+            var jsonBody = JSON.parse(chunk);
+            // console.log(jsonBody);
+            expect(jsonBody.error).to.be.false;
+            expect(jsonBody.message).to.be.defined;
+            expect(jsonBody.token).to.be.defined;
+            adminToken = jsonBody.token;
+            continueNowThatAdminTokenIsKnown();
+        }
+    );
+}
+
+function continueNowThatAdminTokenIsKnown() {
+    require('mongoose').connection.dropDatabase(testDbName);
+    createNewUserAsAdmin(['Tweeny', 'Peled'], 'tweeny@t.com', 'ttt');
+}
+
+getAdminToken();
