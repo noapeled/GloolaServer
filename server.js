76720cee7ac12171241c076e5c98847d15d0d38e
noapeled <noapeled@gmail.com>
2017-06-07 22:51:50 +0200

    Add log

--- server.js
+++ server.js
@@ -183,6 +183,7 @@ function verifyGoogleToken(token, req, res, next) {
         config.auth.gloolaServerGoogleApiClientId,
         function(err, loginData) {
             if (err) {
+                console.log('Failed to authenticate Google token, error is:', JSON.stringify(err));
                 return res.status(403).json({ error: true, message: err.message });
             }
             var payload = loginData.getPayload();
